1bf18b4c3862b3848a3d2d025cd3ec4923091856
unknown <22444128+jppaled@users.noreply.github.com>
2019-11-11 06:13:52 +0100

    Updates

---
+++ precache-manifest.0152f0a7a9e125ad16341a71bdf8e966.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16653f089fc2e98ef5560e6c0aca9929",
+    "url": "/cookiecrud/index.html"
+  },
+  {
+    "revision": "c7f1094f75179e9b14bb",
+    "url": "/cookiecrud/static/css/main.e37a0505.chunk.css"
+  },
+  {
+    "revision": "c31b5ec3af3dc6f193fa",
+    "url": "/cookiecrud/static/js/2.c8e1dce7.chunk.js"
+  },
+  {
+    "revision": "c7f1094f75179e9b14bb",
+    "url": "/cookiecrud/static/js/main.9259c16e.chunk.js"
+  },
+  {
+    "revision": "c18c4cfe91443246aa0c",
+    "url": "/cookiecrud/static/js/runtime-main.26fe1535.js"
+  }
+]);
